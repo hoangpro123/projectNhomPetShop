58a5f1f2ce4841deeb3a0084b85f4f1bdb9d22fc
hoangpro <huyxuan589@gmail.com>
2019-09-28 07:44:48 +0700

    Beta v0.1

---
+++ app/src/main/java/tdc/edu/vn/project/MainActivity.java
@@ -0,0 +1,15 @@
+package tdc.edu.vn.project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+Button go;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
