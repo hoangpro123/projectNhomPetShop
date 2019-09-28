82934c52ad3f1279a23522961dba926246dbbc93
Nguyễn Thanh Khiêm <ntkhiem99@gmail.com>
2019-09-28 09:43:32 +0700

    firebase

--- app/src/main/java/tdc/edu/vn/project/MainActivity.java
+++ app/src/main/java/tdc/edu/vn/project/MainActivity.java
@@ -2,16 +2,21 @@ package tdc.edu.vn.project;
 
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainActivity extends AppCompatActivity {
-Button go;
+    DatabaseReference db = FirebaseDatabase.getInstance().getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
