023af3349c86cd3bb8ad9896141c92bbf30ab6c9
Nguyễn Thanh Khiêm <ntkhiem99@gmail.com>
2019-09-28 15:47:18 +0700

    test firebase 1

--- app/src/main/java/tdc/edu/vn/project/MainActivity.java
+++ app/src/main/java/tdc/edu/vn/project/MainActivity.java
@@ -1,22 +1,16 @@
 package tdc.edu.vn.project;
-
-
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-
 public class MainActivity extends AppCompatActivity {
-    DatabaseReference db = FirebaseDatabase.getInstance().getReference();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        db.addNguoiMua(new NguoiMua("ntk", "khm", "123", "1", "1", "1", "1"));
     }
 }
