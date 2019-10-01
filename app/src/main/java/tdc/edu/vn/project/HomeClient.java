55ab4563ee4e583e7b780902258aa77f45ea662f
Thanh Phong <32613207+ThanhPhongTP@users.noreply.github.com>
2019-10-01 09:47:04 +0700

    ver3

--- app/src/main/java/tdc/edu/vn/project/HomeClient.java
+++ app/src/main/java/tdc/edu/vn/project/HomeClient.java
@@ -2,13 +2,47 @@ package tdc.edu.vn.project;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 
 public class HomeClient extends AppCompatActivity {
+
+    List<Pet> listPet ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_client);
+
+        listPet = new ArrayList<>();
+        listPet.add(new Pet("The Vegitarian","500000","Description book",R.drawable.logo));
+        listPet.add(new Pet("The Wild Robot","Categorie Book","Description book",R.drawable.logo));
+        listPet.add(new Pet("Maria Semples","Categorie Book","Description book",R.drawable.logo));
+        listPet.add(new Pet("The Martian","Categorie Book","Description book",R.drawable.logo));
+        listPet.add(new Pet("He Died with...","Categorie Book","Description book",R.drawable.logo));
+        listPet.add(new Pet("The Vegitarian","Categorie Book","Description book",R.drawable.logo));
+        listPet.add(new Pet("The Vegitarian","Categorie Book","Description book",R.drawable.logo));
+        listPet.add(new Pet("The Wild Robot","Categorie Book","Description book",R.drawable.logo));
+        listPet.add(new Pet("Maria Semples","Categorie Book","Description book",R.drawable.logo));
+        listPet.add(new Pet("The Martian","Categorie Book","Description book",R.drawable.logo));
+        listPet.add(new Pet("He Died with...","Categorie Book","Description book",R.drawable.logo));
+        listPet.add(new Pet("The Vegitarian","Categorie Book","Description book",R.drawable.logo));
+        listPet.add(new Pet("The Vegitarian","Categorie Book","Description book",R.drawable.logo));
+        listPet.add(new Pet("The Wild Robot","Categorie Book","Description book",R.drawable.logo));
+        listPet.add(new Pet("Maria Semples","Categorie Book","Description book",R.drawable.logo));
+        listPet.add(new Pet("The Martian","Categorie Book","Description book",R.drawable.logo));
+        listPet.add(new Pet("He Died with...","Categorie Book","Description book",R.drawable.logo));
+        listPet.add(new Pet("The Vegitarian","Categorie Book","Description book",R.drawable.logo));
+
+        RecyclerView myrv = (RecyclerView) findViewById(R.id.recyclerview);
+        RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(this,listPet);
+        myrv.setLayoutManager(new GridLayoutManager(this,2));
+        myrv.setAdapter(myAdapter);
+
     }
 }
---
+++ app/src/main/java/tdc/edu/vn/project/RecyclerViewAdapter.java
@@ -0,0 +1,88 @@
+package tdc.edu.vn.project;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+
+    private Context mContext ;
+    private List<Pet> mData ;
+
+    public RecyclerViewAdapter(Context mContext, List<Pet> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view ;
+        LayoutInflater mInflater = LayoutInflater.from(mContext);
+        view = mInflater.inflate(R.layout.card_view_item,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+
+        holder.tv_pet_title.setText(mData.get(position).getTitle());
+        holder.tv_price.setText(mData.get(position).getPrice());
+        holder.img_pet.setImageResource(mData.get(position).getThumbnail());
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(mContext,PetActivity.class);
+
+                // passing data to the book activity
+                intent.putExtra("Title",mData.get(position).getTitle());
+                intent.putExtra("Description",mData.get(position).getDescription());
+                intent.putExtra("Thumbnail",mData.get(position).getThumbnail());
+                // start the activity
+                mContext.startActivity(intent);
+
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tv_pet_title;
+        ImageView img_pet;
+        CardView cardView ;
+        TextView tv_price;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            tv_pet_title = (TextView) itemView.findViewById(R.id.pettitle) ;
+            tv_price = (TextView) itemView.findViewById(R.id.price);
+            img_pet = (ImageView) itemView.findViewById(R.id.petimg);
+            cardView = (CardView) itemView.findViewById(R.id.cardview_id);
+
+
+        }
+    }
+
+
+}
