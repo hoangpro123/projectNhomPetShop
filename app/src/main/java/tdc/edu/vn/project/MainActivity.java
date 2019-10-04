ebfc91299720978d2a8165aea75247d49229dd79
Nguyễn Thanh Khiêm <ntkhiem99@gmail.com>
2019-10-04 08:17:47 +0700

    update

--- app/src/main/java/tdc/edu/vn/project/MainActivity.java
+++ app/src/main/java/tdc/edu/vn/project/MainActivity.java
@@ -1,8 +1,14 @@
 package tdc.edu.vn.project;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.lang.reflect.Field;
+import java.util.Properties;
 
 import tdc.edu.vn.project.Model.DanhGia;
 import tdc.edu.vn.project.Model.DanhSachDen;
@@ -22,6 +28,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
     }
 
+    public Properties parsePropertiesString(String s) {
+        // grr at load() returning void rather than the Properties object
+        // so this takes 3 lines instead of "return new Properties().load(...);"
+        final Properties p = new Properties();
+        try {
+            p.load(new StringReader(s));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return p;
+    }
 }
