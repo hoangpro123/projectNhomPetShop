38f071e0435f2b66e7b8ed0ad5a92e334e1f1e8c
Nguyễn Thanh Khiêm <ntkhiem99@gmail.com>
2019-10-05 08:03:35 +0700

    sua kieu du lieu cac model

--- app/src/main/java/tdc/edu/vn/project/Model/HoaHong.java
+++ app/src/main/java/tdc/edu/vn/project/Model/HoaHong.java
@@ -1,9 +1,13 @@
 package tdc.edu.vn.project.Model;
 
-public class HoaHong extends PetShopModel{
-    String ty_le, thoi_gian_dong_tien, so_tien;
+import java.util.Date;
 
-    public HoaHong(String ty_le, String thoi_gian_dong_tien, String so_tien) {
+public class HoaHong extends PetShopModel{
+    Float ty_le;
+    Date thoi_gian_dong_tien;
+    Double so_tien;
+
+    public HoaHong(Float ty_le, Date thoi_gian_dong_tien, Double so_tien) {
         this.ty_le = ty_le;
         this.thoi_gian_dong_tien = thoi_gian_dong_tien;
         this.so_tien = so_tien;
@@ -12,37 +16,28 @@ public class HoaHong extends PetShopModel{
     public HoaHong() {
     }
 
-    @Override
-    public String getId() {
-        return super.getId();
-    }
 
-    @Override
-    public void setId(String id) {
-        super.setId(id);
-    }
-
-    public String getTy_le() {
+    public Float getTy_le() {
         return ty_le;
     }
 
-    public void setTy_le(String ty_le) {
+    public void setTy_le(Float ty_le) {
         this.ty_le = ty_le;
     }
 
-    public String getThoi_gian_dong_tien() {
+    public Date getThoi_gian_dong_tien() {
         return thoi_gian_dong_tien;
     }
 
-    public void setThoi_gian_dong_tien(String thoi_gian_dong_tien) {
+    public void setThoi_gian_dong_tien(Date thoi_gian_dong_tien) {
         this.thoi_gian_dong_tien = thoi_gian_dong_tien;
     }
 
-    public String getSo_tien() {
+    public Double getSo_tien() {
         return so_tien;
     }
 
-    public void setSo_tien(String so_tien) {
+    public void setSo_tien(Double so_tien) {
         this.so_tien = so_tien;
     }
 }
