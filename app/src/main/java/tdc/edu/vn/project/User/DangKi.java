42e3cada3a44e4be1dea40a1f9ef69ee0cf4d58e
hoang <huyxuan589@gmail.com>
2019-10-07 07:31:34 +0700

    VoXuanHoang beta v0.95 Fix bug cant run layout Login # Conflicts: #	app/build.gradle #	app/src/main/AndroidManifest.xml #	app/src/main/java/tdc/edu/vn/project/MainActivity.java #	app/src/main/java/tdc/edu/vn/project/Model/NguoiMua.java #	app/src/main/java/tdc/edu/vn/project/PetShopFireBase.java #	app/src/main/res/layout/layout_dangki.xml #	app/src/main/res/layout/layout_dangnhap.xml #	app/src/main/res/values/strings.xml

--- app/src/main/java/tdc/edu/vn/project/User/DangKi.java
+++ app/src/main/java/tdc/edu/vn/project/User/DangKi.java
@@ -1,5 +1,7 @@
 package tdc.edu.vn.project.User;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 
 
@@ -12,6 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import tdc.edu.vn.project.Model.NguoiMua;
@@ -35,20 +38,31 @@ public class DangKi  extends AppCompatActivity {
         tao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (pass.getText().toString().equals(repass.getText().toString())) {
-                    int re = group.getCheckedRadioButtonId();
-                    gender = findViewById(re);
-                    String Name = "";
+                if (ho.getText().toString() != null && ten.getText().toString() != null && mail.getText().toString() != null && pass.getText().toString() != null && repass.getText().toString() != null && sdt.getText().toString() != null && diachi.getText().toString() != null) {
+                    if (pass.getText().toString().equals(repass.getText().toString())) {
+                        if (sdt.length() == 10) {
+                            int re = group.getCheckedRadioButtonId();
+                            gender = findViewById(re);
+                            String Name = "";
                             Name += ho.getText();
-                    Name += ten.getText();
-                    NguoiMua nm = new NguoiMua(Name, mail.getText().toString(), pass.getText().toString(), sdt.getText().toString(), diachi.getText().toString(), " " ,gender.getText().toString());
-                    PetShopFireBase.pushItem(nm, PetShopFireBase.TABLE_NGUOI_MUA);
-                    Toast.makeText(DangKi.this,"dktc", Toast.LENGTH_LONG).show();
-                }else{
-                    String a = "";
-                    a += pass.getText().toString();
-                    a += repass.getText().toString();
-                    Toast.makeText(DangKi.this,a, Toast.LENGTH_LONG).show();
+                            Name += ten.getText();
+                            NguoiMua nm = new NguoiMua(Name, mail.getText().toString(), pass.getText().toString(), sdt.getText().toString(), diachi.getText().toString(), " ", gender.getText().toString());
+                            PetShopFireBase.pushItem(nm, PetShopFireBase.TABLE_NGUOI_MUA);
+                            dialog();
+                            Toast.makeText(DangKi.this, "dktc", Toast.LENGTH_LONG).show();
+                        } else {
+                            dialogfail();
+                        }
+                    } else {
+                        String a = "";
+                        a += pass.getText().toString();
+                        a += repass.getText().toString();
+
+                        //Toast.makeText(DangKi.this,a, Toast.LENGTH_LONG).show();
+                    }
+                }else {
+                    dialognull();
+
                 }
             }
         });
@@ -71,6 +85,60 @@ public class DangKi  extends AppCompatActivity {
 
             tao = findViewById(R.id.btnTao);
 
+    }
+    private void dialog(){
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_custom);
+        Button XacNhan = (Button)dialog.findViewById(R.id.xacnhan);
+        XacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DangKi.this, Login.class);
+                intent.putExtra("user", mail.getText().toString());
+                intent.putExtra("pass", pass.getText().toString());
+                startActivity(intent);
+            }
+        });
+        dialog.show();
+    }
+    private void dialogfail(){
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_custom);
+        Button XacNhan = (Button)dialog.findViewById(R.id.xacnhan);
+        final TextView view = (TextView)dialog.findViewById(R.id.tt);
+        view.setText("SDT phải có 10 kí tự và bắt đầu bằng số 0");
+        XacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              dialog.cancel();
+            }
+        });
+
+        dialog.show();
+    }
+    private void dialognull(){
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_custom);
+        Button XacNhan = (Button)dialog.findViewById(R.id.xacnhan);
+        final TextView view = (TextView)dialog.findViewById(R.id.tt);
+        view.setText("Phải Nhập hết Các ô");
+        XacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.cancel();
+            }
+        });
+
+        dialog.show();
+    }
+    public boolean checkPhone()
+    {
+       String num = "";
+                   if (sdt.getText().toString().charAt(0) == 0){
+                       return true;
+                   }else {
+                       return false;
+                   }
     }
 }
 
