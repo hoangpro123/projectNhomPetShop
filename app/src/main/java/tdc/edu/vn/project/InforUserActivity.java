9911458380e7a2473d3446671028602dbe454cab
Thanh Phong <32613207+ThanhPhongTP@users.noreply.github.com>
2019-10-14 09:07:50 +0700

    Giao diện chỉnh sửa thông tin

---
+++ app/src/main/java/tdc/edu/vn/project/InforUserActivity.java
@@ -0,0 +1,14 @@
+package tdc.edu.vn.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InforUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infor_user);
+    }
+}
