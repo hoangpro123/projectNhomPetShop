a2eb27dbe1f63d619a5d524fcded8023a1e08dc7
nhandayna <tim@timrich.org>
2019-10-15 08:21:10 +0700

    Thông tin user hiển thị đơn hàng (adapter)

---
+++ app/src/main/java/tdc/edu/vn/project/ThongTinDonHang.java
@@ -0,0 +1,16 @@
+package tdc.edu.vn.project;
+
+public class ThongTinDonHang {
+    public String tenHang;
+    public String trangThai;
+    public String danhGia;
+
+    public ThongTinDonHang() {
+    }
+
+    public ThongTinDonHang(String tenHang, String trangThai, String danhGia) {
+        this.tenHang = tenHang;
+        this.trangThai = trangThai;
+        this.danhGia = danhGia;
+    }
+}
--- app/src/main/java/tdc/edu/vn/project/ThongTinUser.java
+++ app/src/main/java/tdc/edu/vn/project/ThongTinUser.java
@@ -3,12 +3,39 @@ package tdc.edu.vn.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ThongTinUser extends AppCompatActivity {
-
+    private ListView lv1;
+    ArrayList<ThongTinDonHang> data = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_thongtinuser);
+        setControl();
+        setEvent();
+    }
+    public void setControl(){
+        lv1 = findViewById(R.id.lv1);
+    }
+
+    public void setEvent(){
+        khoiTao();
+        AdapterDonHang ad = new AdapterDonHang(this,R.layout.listview_donhang_trangthai,data);
+        lv1.setAdapter(ad);
+    }
+
+    public void khoiTao(){
+        data.add(new ThongTinDonHang("Mèo Anh Lông ngắn","Hoàn Thành","Đánh giá"));
+        data.add(new ThongTinDonHang("Cậu Vàng","Đang giao","Đánh giá"));
+        data.add(new ThongTinDonHang("Cậu Vàng","Đang giao","Đánh giá"));
+        data.add(new ThongTinDonHang("Cậu Vàng","Đang giao","Đánh giá"));
+        data.add(new ThongTinDonHang("Cậu Vàng","Đang giao","Đánh giá"));
+        data.add(new ThongTinDonHang("Cậu Vàng","Đang giao","Đánh giá"));
+
+
     }
 }
