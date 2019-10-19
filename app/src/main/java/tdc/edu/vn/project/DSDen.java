57c7a0f78cde912f12d31926adc3269751500738
nhandayna <tim@timrich.org>
2019-10-19 09:36:41 +0700

    Thông tin hiển thị list danh sach den có(adapter)

---
+++ app/src/main/java/tdc/edu/vn/project/DSDen.java
@@ -0,0 +1,18 @@
+package tdc.edu.vn.project;
+
+public class DSDen {
+    public String hinhNguoiDanhSachDen;
+    public String hoTen;
+    public String email;
+    public String soDienThoai;
+
+    public DSDen() {
+    }
+
+    public DSDen(String hinhNguoiDanhSachDen, String hoTen, String email, String soDienThoai) {
+        this.hinhNguoiDanhSachDen = hinhNguoiDanhSachDen;
+        this.hoTen = hoTen;
+        this.email = email;
+        this.soDienThoai = soDienThoai;
+    }
+}
