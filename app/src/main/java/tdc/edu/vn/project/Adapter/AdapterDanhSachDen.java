ebc98fca790c823fa1d6bad2673ee233d89c18b7
hoangvo31199 <hoangvo31199@gmail.com>
2019-10-25 09:21:13 +0700

    Danh sach den với nhiều đối tượng + update đăng nhập version: omega v0.2

--- app/src/main/java/tdc/edu/vn/project/Adapter/AdapterDanhSachDen.java
+++ app/src/main/java/tdc/edu/vn/project/Adapter/AdapterDanhSachDen.java
@@ -9,32 +9,41 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import androidx.annotation.NonNull;
 
+import java.text.Normalizer;
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.regex.Pattern;
 
 import tdc.edu.vn.project.Model.DanhSachDen;
 import tdc.edu.vn.project.Model.NguoiBan;
 import tdc.edu.vn.project.Model.NguoiGiao;
 import tdc.edu.vn.project.Model.NguoiMua;
+import tdc.edu.vn.project.Model.SanPham;
 import tdc.edu.vn.project.PetShopFireBase;
 import tdc.edu.vn.project.R;
 import tdc.edu.vn.project.Screen.DanhSachDenScreen;
+
+import static java.util.Locale.getDefault;
 
 public class AdapterDanhSachDen extends ArrayAdapter<DanhSachDen> {
 
     Activity context;
     int layoutrs;
     ArrayList<DanhSachDen> data;
-
+    ArrayList<DanhSachDen> mdata = new ArrayList<>();
     public AdapterDanhSachDen(@NonNull Activity context, int resource, @NonNull ArrayList<DanhSachDen> objects) {
         super(context, resource, objects);
         this.context = context;
         this.layoutrs = resource;
         this.data = objects;
+        this.mdata.addAll(data);
     }
 
     public View getView(int position, View cView, ViewGroup parent){
@@ -47,6 +56,7 @@ public class AdapterDanhSachDen extends ArrayAdapter<DanhSachDen> {
             holder = new CountryHolder();
             holder.ivHinh = (ImageView) row.findViewById(R.id.ivHinh);
             holder.txtHoTen = (TextView) row.findViewById(R.id.txtHoTen);
+
             holder.txtEmail = (TextView) row.findViewById(R.id.txtEmail);
             holder.txtSDT = (TextView) row.findViewById(R.id.txtSDT);
             holder.Xoa = (Button)row.findViewById(R.id.btnXoa);
@@ -61,6 +71,7 @@ public class AdapterDanhSachDen extends ArrayAdapter<DanhSachDen> {
         if (PetShopFireBase.TABLE_NGUOI_MUA.status_data || PetShopFireBase.TABLE_NGUOI_BAN.status_data){
             ArrayList<NguoiMua> dataNguoiMua = (ArrayList<NguoiMua>) PetShopFireBase.TABLE_NGUOI_MUA.data;
             ArrayList<NguoiBan> dataNguoiBan = (ArrayList<NguoiBan>) PetShopFireBase.TABLE_NGUOI_BAN.data;
+
             for (int i = 0; i < dataNguoiMua.size(); i++){
                 if (dataNguoiMua.get(i).getId().equals(item.getId_nguoi_mua())){
                     holder.txtHoTen.setText(dataNguoiMua.get(i).getName());
@@ -69,7 +80,8 @@ public class AdapterDanhSachDen extends ArrayAdapter<DanhSachDen> {
                     holder.Xoa.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            PetShopFireBase.removeItem(item.getId_nguoi_mua(), PetShopFireBase.eTable.DanhSachDen);
+                            PetShopFireBase.removeItem(item.getId(), PetShopFireBase.eTable.DanhSachDen);
+                            Toast.makeText(context, "deleted", Toast.LENGTH_SHORT).show();
                             notifyDataSetChanged();
                         }
                     });
@@ -84,7 +96,8 @@ public class AdapterDanhSachDen extends ArrayAdapter<DanhSachDen> {
                     holder.Xoa.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            PetShopFireBase.removeItem(item.getId_nguoi_mua(), PetShopFireBase.eTable.DanhSachDen);
+                            PetShopFireBase.removeItem(item.getId(), PetShopFireBase.eTable.DanhSachDen);
+                            Toast.makeText(context, "deleted", Toast.LENGTH_SHORT).show();
                             notifyDataSetChanged();
                         }
                     });
@@ -97,7 +110,31 @@ public class AdapterDanhSachDen extends ArrayAdapter<DanhSachDen> {
         return row;
     }
 
+    public void filter(String charText) {
+        charText = charText.toLowerCase(getDefault());
+        //removeAccent(charText);
+        data.clear();
+        if(charText.length() == 0){
+            data.addAll(mdata);
+        }else {
+            for (DanhSachDen danhSachDen : mdata){
+                if(removeAccent(danhSachDen.getId_nguoi_ban()).toLowerCase(Locale.getDefault()).contains(charText) || removeAccent(danhSachDen.getId_nguoi_mua()).toLowerCase(Locale.getDefault()).contains(charText)){
+                    data.add(danhSachDen);
+                }
+            }
+        }
+        notifyDataSetChanged();
+    }
+
+    public static String removeAccent(String s) {
+
+        String temp = Normalizer.normalize(s, Normalizer.Form.NFD);
+        Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+        return pattern.matcher(temp).replaceAll("");
+    }
+
     static class CountryHolder{
+        SearchView searchView;
         ImageView ivHinh;
         TextView txtHoTen;
         TextView txtEmail;
--- app/src/main/java/tdc/edu/vn/project/Screen/DanhSachDenScreen.java
+++ app/src/main/java/tdc/edu/vn/project/Screen/DanhSachDenScreen.java
@@ -3,6 +3,7 @@ package tdc.edu.vn.project.Screen;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -12,11 +13,14 @@ import java.util.ArrayList;
 
 import tdc.edu.vn.project.Adapter.AdapterDanhSachDen;
 import tdc.edu.vn.project.Model.DanhSachDen;
+import tdc.edu.vn.project.Pet;
 import tdc.edu.vn.project.PetShopFireBase;
 import tdc.edu.vn.project.R;
 
 public class DanhSachDenScreen extends AppCompatActivity {
     private ListView lv3;
+    AdapterDanhSachDen ad;
+    SearchView searchView;
     ArrayList<DanhSachDen> data;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,31 +32,42 @@ public class DanhSachDenScreen extends AppCompatActivity {
 
     public void setControl(){
         lv3 = (ListView) findViewById(R.id.lv3);
+        searchView = (SearchView)findViewById(R.id.sreach);
     }
 
     public void setEvent(){
         khoiTao();
-        Toast.makeText(DanhSachDenScreen.this, PetShopFireBase.TABLE_DANH_SACH_DEN.data.toString(), Toast.LENGTH_SHORT).show();
         final Handler handler = new Handler();
         handler.post(new Runnable() {
             @Override
             public void run() {
                 if (PetShopFireBase.TABLE_DANH_SACH_DEN.status_data){
 
-
                     data = (ArrayList<DanhSachDen>) PetShopFireBase.TABLE_DANH_SACH_DEN.data;
-                    AdapterDanhSachDen ad = new AdapterDanhSachDen(DanhSachDenScreen.this,R.layout.item_danh_sach_den,data);
-                    Toast.makeText(getApplication(), data.get(0).getId_nguoi_ban(), Toast.LENGTH_SHORT).show();
+                     ad = new AdapterDanhSachDen(DanhSachDenScreen.this,R.layout.item_danh_sach_den,data);
+                    //Toast.makeText(getApplication(), data.get(0).getId_nguoi_ban(), Toast.LENGTH_SHORT).show();
                     lv3.setAdapter(ad);
+                    handler.postDelayed(this, 1000);
 
                 }else {
                     handler.postDelayed(this, 1000);
-                    Toast.makeText(getApplication(), "Không Có dữ liệu", Toast.LENGTH_SHORT).show();
+                  //  Toast.makeText(getApplication(), "Không Có dữ liệu", Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                ad.filter(newText);
+                return false;
+            }
+        });
 
 
     }
