240b2a699076bfe3eaa2543d2833815891759ef7
Nguyễn Thanh Khiêm <ntkhiem99@gmail.com>
2019-10-29 08:24:26 +0700

    sửa database cho chọn nhiều ảnh

--- app/src/main/java/tdc/edu/vn/project/Model/SanPham.java
+++ app/src/main/java/tdc/edu/vn/project/Model/SanPham.java
@@ -1,14 +1,16 @@
 package tdc.edu.vn.project.Model;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class SanPham extends PetShopModel{
-    String name, description, image, id_nguoi_ban;
+    String name, description, id_nguoi_ban;
+    ArrayList<String> image;
     Double price;
     Date ngay_dang_ban;
 
 
-    public SanPham(String name, String description, String image, String id_nguoi_ban, Double price, Date ngay_dang_ban) {
+    public SanPham(String name, String description, ArrayList<String> image, String id_nguoi_ban, Double price, Date ngay_dang_ban) {
         this.name = name;
         this.description = description;
         this.image = image;
@@ -36,20 +38,20 @@ public class SanPham extends PetShopModel{
         this.description = description;
     }
 
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
     public String getId_nguoi_ban() {
         return id_nguoi_ban;
     }
 
     public void setId_nguoi_ban(String id_nguoi_ban) {
         this.id_nguoi_ban = id_nguoi_ban;
+    }
+
+    public ArrayList<String> getImage() {
+        return image;
+    }
+
+    public void setImage(ArrayList<String> image) {
+        this.image = image;
     }
 
     public Double getPrice() {
