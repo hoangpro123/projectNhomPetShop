ef38b5f90f50a257a6dc92bcc2d5621c62a0ff4e
Thanh Phong <32613207+ThanhPhongTP@users.noreply.github.com>
2019-11-01 14:18:33 +0700

    fix loi ghep code

---
+++ app/src/main/java/tdc/edu/vn/project/Model/GioHang.java
@@ -0,0 +1,39 @@
+package tdc.edu.vn.project.Model;
+
+public class GioHang extends PetShopModel {
+    String id_nguoi_mua, id_san_pham;
+    Integer so_luong;
+
+    public GioHang(String id_nguoi_mua, String id_san_pham, Integer so_luong) {
+        this.id_nguoi_mua = id_nguoi_mua;
+        this.id_san_pham = id_san_pham;
+        this.so_luong = so_luong;
+    }
+
+    public GioHang() {
+    }
+
+    public String getId_nguoi_mua() {
+        return id_nguoi_mua;
+    }
+
+    public void setId_nguoi_mua(String id_nguoi_mua) {
+        this.id_nguoi_mua = id_nguoi_mua;
+    }
+
+    public String getId_san_pham() {
+        return id_san_pham;
+    }
+
+    public void setId_san_pham(String id_san_pham) {
+        this.id_san_pham = id_san_pham;
+    }
+
+    public Integer getSo_luong() {
+        return so_luong;
+    }
+
+    public void setSo_luong(Integer so_luong) {
+        this.so_luong = so_luong;
+    }
+}
---
+++ app/src/main/java/tdc/edu/vn/project/Screen/GioHangActivity.java
@@ -0,0 +1,17 @@
+package tdc.edu.vn.project.Screen;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import tdc.edu.vn.project.R;
+
+class GioHangActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_giohang);
+
+//        setControl();
+//        setEvent();
+    }
+}
