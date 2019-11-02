cca18d26d672c38880146cf79e838147836e0919
Nguyễn Thanh Khiêm <ntkhiem99@gmail.com>
2019-11-02 18:47:22 +0700

    Xoá code thừa

--- app/src/main/java/tdc/edu/vn/project/Screen/Login.java
+++ app/src/main/java/tdc/edu/vn/project/Screen/Login.java
@@ -6,7 +6,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import android.os.Handler;
-import android.se.omapi.Session;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
